973644dd7697bc27c443241a08b8eefcce545cf1
Bellkross <vasilenkowm@yandex.ua>
2018-02-20 02:30:30 +0200

    added normal spimi with list of BRs

--- src/Helper.kt
+++ src/Helper.kt
@@ -1,317 +1,92 @@
 import java.io.File
 import java.util.*
-import kotlin.collections.HashMap
-import kotlin.collections.HashSet
+import kotlin.collections.ArrayList
 
 class Helper {
 
-    fun permutationIndex(inputString: String): MutableList<String> {
-        var string = "$inputString$"
-        var i = string.length
-        val result: MutableList<String> = LinkedList()
-        while (--i >= 0) {
-            result.add(string)
-            string += string[0]
-            string = string.substring(1)
-        }
 
-        //hello -> hello$, ello$h, llo$he, lo$hel, o$hell, $hello
-        return result
-    }
+    fun merge(files: Array<File>) {
+        val brs = files.map { it.bufferedReader() }
+        File("r.txt").createNewFile()
+        val bw = File("r.txt").bufferedWriter()
+        val strings = ArrayList<String?>()
+        val index: TreeMap<String, TreeSet<Int>> = TreeMap()
 
-    fun changeJoker(inputString: String): String {
-        var string = "$$inputString"
-        while (string[string.length - 1] != '*') {
-            string += string[0]
-            string = string.substring(1)
-        }
-
-        return string
-    }
-
-    //for normal words
-    fun kgramIndex(inputString: String, k: Int): MutableList<String> {
-        var string = "$$inputString$"
-        var i = string.length
-        val result: MutableList<String> = LinkedList()
-        while (--i >= k - 1) {
-            result.add(string.substring(0, k))
-            string = string.substring(1)
-        }
-
-        //castle -> $ca, cas, ast, stl, tle, le$
-        return result
-    }
-
-    //for non-ended words
-    fun kgramIndex2(inputString: String, k: Int): MutableList<String> {
-        var string = "$$inputString"
-        var i = string.length
-        val result: MutableList<String> = LinkedList()
-        while (--i >= k - 1) {
-            result.add(string.substring(0, k))
-            string = string.substring(1)
-        }
-
-        return result
-    }
-
-    //for non-started words
-    fun kgramIndex3(inputString: String, k: Int): MutableList<String> {
-        var string = "$inputString$"
-        var i = string.length
-        val result: MutableList<String> = LinkedList()
-        while (--i >= k - 1) {
-            result.add(string.substring(0, k))
-            string = string.substring(1)
-        }
-
-        return result
-    }
-
-    fun mergeFiles2(path1: String, path2: String, resPath: String) {
-        val file1 = File(path1)
-        val file2 = File(path2)
-
-        val res = HashMap<String, HashSet<Int>>()
-
-        var string1: String? = ""
-        var string2: String? = ""
-        var word1 = ""
-        var word2 = ""
-        var treeSetWords = TreeSet<String>()
-
-        var resString = ""
-
-        val fileResult = File(resPath)
-        file1.bufferedReader().use { br1 ->
-            file2.bufferedReader().use { br2 ->
-
-                string1 = br1.readLine()
-                string2 = br2.readLine()
-                while (string1 != null && string2 != null) {
-
-                    word1 = getWordFromCoordinateIndex(string1!!)
-                    word2 = getWordFromCoordinateIndex(string2!!)
-                    treeSetWords.add(word1)
-                    treeSetWords.add(word2)
-                    if (word1 == word2) {
-                        res[word1] = parseStringToMutableList(string1!!)
-                        parseStringToMutableList(string2!!).forEach {
-                            res[word1]!!.add(it)
-                        }
-                        resString += "$word1 -> ${res[word1].toString()}\n"
-/*
-                        fileResult.bufferedWriter().use { out ->
-                            out.write("$word1 -> ${res[word1].toString()}\n")
-                        }
-*/
-                        string1 = br1.readLine()
-                        string2 = br2.readLine()
-                    } else {
-                        when {
-                            treeSetWords.last()==word2 -> {
-                                res[word1] = parseStringToMutableList(string1!!)
-                                /*
-                                fileResult.bufferedWriter().use { out ->
-                                    out.write("$word1 -> ${res[word1].toString()}\n")
-                                }
-                                */
-                                resString += "$word1 -> ${res[word1].toString()}\n"
-                                string1 = br1.readLine()
-                            }
-                            else -> {
-                                res[word2] = parseStringToMutableList(string2!!)
-                                /*
-                                fileResult.bufferedWriter().use { out ->
-                                    out.write("$word2 -> ${res[word2].toString()}\n")
-                                }
-                                */
-                                resString += "$word2 -> ${res[word2].toString()}\n"
-                                string2 = br2.readLine()
-                            }
-                        }
-                    }
-                    treeSetWords.clear()
-                }
-
-                while (string1 == null && string2 != null) {
-                    word2 = getWordFromCoordinateIndex(string2!!)
-                    res[word2] = parseStringToMutableList(string2!!)
-                    //fileResult.appendText("$word2 -> ${res[word2].toString()}\n")
-                    resString += "$word2 -> ${res[word2].toString()}\n"
-                    string2 = br2.readLine()
-                }
-                while (string1 != null && string2 == null) {
-                    word1 = getWordFromCoordinateIndex(string1!!)
-                    res[word1] = parseStringToMutableList(string1!!)
-                    //fileResult.appendText("$word1 -> ${res[word1].toString()}\n")
-                    resString += "$word1 -> ${res[word1].toString()}\n"
-                    string1 = br1.readLine()
-                }
-
-            }
-        }
-        fileResult.bufferedWriter().use { out ->
-            out.write(resString)
-        }
-
-    }
-
-    //p1&p2 -> p1
-    fun mergeFiles(path1: String, path2: String, resPath: String) {
-        val file1 = File(path1)
-        val file2 = File(path2)
-
-        val res = HashMap<String, HashMap<Int, HashSet<Int>>>()
-
-        var string1: String? = ""
-        var string2: String? = ""
-        var word1 = ""
-        var word2 = ""
-        var treeSetWords = TreeSet<String>()
-
-        var resString = ""
-
-        val fileResult = File(resPath)
-        file1.bufferedReader().use { br1 ->
-            file2.bufferedReader().use { br2 ->
-
-                string1 = br1.readLine()
-                string2 = br2.readLine()
-                while (string1 != null && string2 != null) {
-
-                    word1 = getWordFromCoordinateIndex(string1!!)
-                    word2 = getWordFromCoordinateIndex(string2!!)
-                    treeSetWords.add(word1)
-                    treeSetWords.add(word2)
-                    if (word1 == word2) {
-                        res[word1] = parseStringToCoordinateIndex(string1!!)
-                        parseStringToCoordinateIndex(string2!!).keys.forEach {
-                            res[word1]!![it] = parseStringToCoordinateIndex(string2!!)[it]!!
-                        }
-                        resString += "$word1 -> ${res[word1].toString()}\n"
-/*
-                        fileResult.bufferedWriter().use { out ->
-                            out.write("$word1 -> ${res[word1].toString()}\n")
-                        }
-*/
-                        string1 = br1.readLine()
-                        string2 = br2.readLine()
-                    } else {
-                        when {
-                            treeSetWords.last()==word2 -> {
-                                res[word1] = parseStringToCoordinateIndex(string1!!)
-                                /*
-                                fileResult.bufferedWriter().use { out ->
-                                    out.write("$word1 -> ${res[word1].toString()}\n")
-                                }
-                                */
-                                resString += "$word1 -> ${res[word1].toString()}\n"
-                                string1 = br1.readLine()
-                            }
-                            else -> {
-                                res[word2] = parseStringToCoordinateIndex(string2!!)
-                                /*
-                                fileResult.bufferedWriter().use { out ->
-                                    out.write("$word2 -> ${res[word2].toString()}\n")
-                                }
-                                */
-                                resString += "$word2 -> ${res[word2].toString()}\n"
-                                string2 = br2.readLine()
-                            }
-                        }
-                    }
-                    treeSetWords.clear()
-                }
-
-                while (string1 == null && string2 != null) {
-                    word2 = getWordFromCoordinateIndex(string2!!)
-                    res[word2] = parseStringToCoordinateIndex(string2!!)
-                    //fileResult.appendText("$word2 -> ${res[word2].toString()}\n")
-                    resString += "$word2 -> ${res[word2].toString()}\n"
-                    string2 = br2.readLine()
-                }
-                while (string1 != null && string2 == null) {
-                    word1 = getWordFromCoordinateIndex(string1!!)
-                    res[word1] = parseStringToCoordinateIndex(string1!!)
-                    //fileResult.appendText("$word1 -> ${res[word1].toString()}\n")
-                    resString += "$word1 -> ${res[word1].toString()}\n"
-                    string1 = br1.readLine()
-                }
-
-            }
-        }
-        fileResult.bufferedWriter().use { out ->
-            out.write(resString)
-        }
-
-    }
-
-    fun getWordFromCoordinateIndex(inputString: String): String = inputString.substring(0, inputString.indexOf(" -> "))
-
-    fun parseStringToCoordinateIndex(inputString: String): HashMap<Int, HashSet<Int>> {
-
-        val result: HashMap<Int, HashSet<Int>> = HashMap()
-        var string = inputString
-        var coordinates: HashSet<Int>
-        var id = 0
-
-        string = string.substring(string.indexOf("{") + "{".length)
-
-        while (string.indexOf("}") != 0) {
-            id = Integer.parseInt(string.substring(
-                    startIndex = 0,
-                    endIndex = string.indexOf("=[")))
-            string = string.substring(string.indexOf("=["))
-            coordinates = parseStringToMutableList(string.substring(string.indexOf("=["), string.indexOf("]") + 1))
-            if (string.contains("], ")) {
-                string = string.substring(string.indexOf("], ") + "], ".length)
-            } else {
-                string = string.substring(string.indexOf("]") + "]".length)
-            }
-            result[id] = coordinates
-        }
-
-        return result
-    }
-
-    fun parseStringToMutableList(inputString: String): HashSet<Int> {
-
-        if (inputString.length < 3) {
-            return HashSet()
-        }
-        var string = inputString.substring(inputString.indexOf("[") + 1)
-        val result = HashSet<Int>()
-        while (string.indexOf("]") != 0) {
-            string = if (string.contains(", ")) {
-                result.add(Integer.parseInt(string.substring(0, string.indexOf(", "))))
-                string.substring(string.indexOf(", ") + ", ".length)
-            } else {
-                result.add(Integer.parseInt(string.substring(0, string.indexOf("]"))))
-                string.substring(string.indexOf("]"))
-            }
-        }
-
-        return result
-    }
-
-    private fun intersect(vector1: MutableList<Int>, vector2: MutableList<Int>, distance: Int): MutableList<Int> {
-        val resList: MutableList<Int> = LinkedList()
+        var word = ""
         var i = 0
-        var j = 0
-        while (i < vector1.size && j < vector2.size) {
-            when {
-                distance >= Math.abs(vector2[j] - vector1[i]) -> {
-                    resList.add(vector1[i++])
-                    ++j
-                }
-                distance < Math.abs(vector2[j] - vector1[i]) -> ++i
-                else -> ++j
-            }
+        while (i++ < brs.size) {
+            strings.add("")
         }
 
-        return resList
+        brs.forEachIndexed { i, br ->
+            strings[i] = br.readLine()
+        }
+
+        while (haveOneNotNull(strings)) {
+            i = 0
+            while (i < strings.size) {
+                if (strings[i] != null) {
+                    word = getWordFromIndex(strings[i])
+                    if (index.containsKey(word)) {
+                        index[word]!!.addAll(parseStringToIndex(strings[i]!!))
+                    } else {
+                        index[word] = TreeSet()
+                        index[word]!!.addAll(parseStringToIndex(strings[i]!!))
+                    }
+                }
+                i++
+            }
+
+            if (allEquals(strings)) {
+                brs.forEachIndexed { i, _ ->
+                    if (strings[i] != null)
+                        strings[i] = brs[i].readLine()
+                }
+
+                index.clear()
+            } else {
+
+                bw.append("${index.keys.first()} -> ${index[index.keys.first()]}\n")
+                bw.flush()
+                i = 0
+                while (i < strings.size) {
+                    if (strings[i] != null &&
+                            getWordFromIndex(strings[i]) == index.keys.first()) {
+                        strings[i] = brs[i].readLine()
+                    }
+                    i++
+                }
+                index[index.keys.first()]!!.clear()
+                index.remove(index.keys.first())
+            }
+        }
+        brs.forEach { it.close() }
+        bw.close()
+    }
+
+    private fun allEquals(strings: ArrayList<String?>): Boolean {
+        strings.forEach { it1 ->
+            strings.forEach { it2 ->
+                if (it1 != it2) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private fun haveOneNotNull(strings: ArrayList<String?>): Boolean {
+        strings.forEach { if (it != null) return true }
+        return false
+    }
+
+    fun getWordFromIndex(inputString: String?): String = inputString!!.substring(0, inputString.indexOf(" ->"))
+
+    fun parseStringToIndex(inputString: String): TreeSet<Int> {
+        val string = inputString.substring(inputString.indexOf("["))
+        val result = TreeSet<Int>()
+        result.addAll(string.removePrefix("[").removeSuffix("]").replace(" ", "").split(",").map { Integer.parseInt(it) })
+        return result
     }
 
 }
--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,5 @@
 import java.io.File
 import java.time.LocalTime
-import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 const val mFilepath = "D:\\IntelIJ\\txtFiles"
 val files = File(mFilepath).listFiles()
@@ -30,11 +27,22 @@ file fileII.txt and last_outputInvertedIndex.txt merged into fileII.txt
 Time -> 9501 sec
  */
 fun main(args: Array<String>) {
+
+    val time1: Long = LocalTime.now().toNanoOfDay()
+
+    //val files = indexing()
+
+    helper.merge(File("D:\\IntelIJ\\Dictionary\\files").listFiles())
+
+    val time2 = LocalTime.now().toNanoOfDay()
+
+    println("Time -> ${(time2 - time1) / 1000000000} sec")
+
+}
+
+fun indexing(): ArrayList<File> {
     val resFiles = ArrayList<File>()
-    var fileInvertedIndex : File
-
-
-    val time1 : Long = LocalTime.now().toNanoOfDay()
+    var fileInvertedIndex: File
 
     val lim = 170544104
     var j = 0
@@ -44,7 +52,7 @@ fun main(args: Array<String>) {
         //        println("free memory -> ${Runtime.getRuntime().freeMemory()}")
         if (Runtime.getRuntime().freeMemory() < lim) {
             println("free memory < $lim bytes")
-            fileInvertedIndex = File("${j}_${i}outputInvertedIndex.txt")
+            fileInvertedIndex = File("D:\\IntelIJ\\Dictionary\\files\\${j}_${i}outputInvertedIndex.txt")
             resFiles.add(fileInvertedIndex)
             fileInvertedIndex.bufferedWriter().use { out ->
                 dictionary.list.forEach { key ->
@@ -57,7 +65,7 @@ fun main(args: Array<String>) {
         }
     }
 
-    fileInvertedIndex = File("last_outputInvertedIndex.txt")
+    fileInvertedIndex = File("D:\\IntelIJ\\Dictionary\\files\\last_outputInvertedIndex.txt")
     resFiles.add(fileInvertedIndex)
     fileInvertedIndex.bufferedWriter().use { out ->
         dictionary.list.forEach { key ->
@@ -65,93 +73,6 @@ fun main(args: Array<String>) {
         }
     }
 
-    println("let's merge!")
-
-    File("fileII.txt").createNewFile()
-    val file = File("fileII.txt")
-    resFiles.forEach {
-        helper.mergeFiles2(file.path, it.path, file.path)
-        println("file ${file.path} and ${it.path} merged into ${file.path}")
-    }
-
-/*
-coordinated index
-    val resFiles = ArrayList<File>()
-    var fileCoordinateInvertedIndex : File
-
-    val lim = 170544104
-    var j = 0
-    files.forEachIndexed { i, file ->
-        dictionary.addDocument(Document(file.absolutePath, i))
-//        println("free memory -> ${Runtime.getRuntime().freeMemory()}")
-        if (Runtime.getRuntime().freeMemory() < lim) {
-            println("free memory < $lim bytes")
-            fileCoordinateInvertedIndex = File("${j}_${i}outputCoordinateInvertedIndex.txt")
-            resFiles.add(fileCoordinateInvertedIndex)
-            fileCoordinateInvertedIndex.bufferedWriter().use { out ->
-                dictionary.list.forEach { key ->
-                    if (!key.isEmpty()) out.write("$key -> ${dictionary.coordinateInvertedIndex[key].toString()}\n")
-                }
-            }
-            dictionary.coordinateInvertedIndex.clear()
-            dictionary.list.clear()
-            j = i
-        }
-    }
-
-
-    fileCoordinateInvertedIndex = File("outputCoordinateInvertedIndex.txt")
-    resFiles.add(fileCoordinateInvertedIndex)
-    fileCoordinateInvertedIndex.bufferedWriter().use { out ->
-        dictionary.list.forEach { key ->
-            if (!key.isEmpty()) out.write("$key -> ${dictionary.coordinateInvertedIndex[key].toString()}\n")
-        }
-    }
-    println("let's merge!")
-
-    File("file.txt").createNewFile()
-    val file = File("file.txt")
-    resFiles.forEach {
-        helper.mergeFiles(file.path, it.path, file.path)
-        println("file ${file.path} and ${it.path} merged into ${file.path}")
-    }
-*/
-
-    val time2 = LocalTime.now().toNanoOfDay()
-
-    println("Time -> ${(time2 - time1)/1000000000} sec")
-    /*
-    val filePermutationIndex = File("outputPermutationIndex.txt")
-    filePermutationIndex.bufferedWriter().use { out ->
-        dictionary.list.forEach { key ->
-            if (!key.isEmpty()) out.write("$key -> ${dictionary.permutationIndex[key].toString()}\n")
-        }
-    }
-
-    val fileTrigramIndex = File("outputTrigramIndex.txt")
-    fileTrigramIndex.bufferedWriter().use { out ->
-        dictionary.trigramList.forEach { key ->
-            if (!key.isEmpty()) out.write("$key -> ${dictionary.trigramIndex[key].toString()}\n")
-        }
-    }
-
-    val fileTree = File("outputTree.txt")
-    fileTree.bufferedWriter().use { out ->
-        out.write("${dictionary.tree}")
-    }
-
-    run {
-        println(dictionary.trigramFind("Gla*st*ne"))
-        println(dictionary.bigramFind("Gla*e"))
-        println( dictionary.find("Харриман вот уже лет", 10) )
-        /*
-        [$Gl, Gla, ne$]
-        {9=[48966, 49649]}
-        [$G, Gl, la, e$]
-        {9=[48966, 49649]}
-        {2=[28]}
-         */
-    }
-    */
+    return resFiles
 
 }
